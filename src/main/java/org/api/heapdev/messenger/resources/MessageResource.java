7b148999b2d14d05a61afbe7e62be01521e68eaf
Abhinav <singh09.abhinav@gmail.com>
2017-07-04 01:37:00 +0530

    Added database class and accesssing data from it.

--- src/main/java/org/api/heapdev/messenger/resources/MessageResource.java
+++ src/main/java/org/api/heapdev/messenger/resources/MessageResource.java
@@ -1,17 +1,24 @@
 package org.api.heapdev.messenger.resources;
 
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.api.heapdev.messenger.model.Message;
+import org.api.heapdev.messenger.service.MessageService;
+
 @Path("/messages")
 public class MessageResource {
 	
+	MessageService messageService = new MessageService();
+	
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String getMessages(){
-		return "My first message";
+	@Produces(MediaType.APPLICATION_XML)
+	public List<Message> getMessages(){
+		return messageService.getAllMesssages();
 	}
 }
--- src/main/java/org/api/heapdev/messenger/service/MessageService.java
+++ src/main/java/org/api/heapdev/messenger/service/MessageService.java
@@ -1,5 +1,26 @@
 package org.api.heapdev.messenger.service;
 
-public class MessageService {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.api.heapdev.messenger.model.Message;
+
+public class MessageService {	
+	
+	private static Map<Long, Message> messages = new HashMap<>();
+	
+	public List<Message> getAllMesssages(){
+		return new ArrayList<Message>(messages.values());
+	}
+	
+	public Message getMessage(Long id){
+		return messages.get(id);
+	}
+	
+	public void addMessage(Message message){
+		
+	}
 
 }
